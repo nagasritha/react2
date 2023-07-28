67c37d880753d06adf17eb14079b9ab7c602db87
nagasritha <eernagasritha@gmail.com>
2023-07-28 15:12:19 +0530

    more

--- src/Components/TrailComponentOne/index.js
+++ src/Components/TrailComponentOne/index.js
@@ -3,14 +3,57 @@ import Image from '../Image'
 import Image2 from '../Image2'
 import './index.css'
 
+let interval1=null
+let interval2=null
+let interval3=null
 class TrailComponentOne extends Component{
+    state={
+        count:0,
+        years:0,
+        experience:0
+    }
+
+    componentDidMount(){
+        this.IncreaseCount()
+    }
+
+    IncreaseCount=()=>{
+        interval1=setInterval(this.changeCount,20)
+        interval2=setInterval(this.changeYears,100)
+        interval3=setInterval(this.changeExperience,100)
+        
+    }
+
+    changeCount=()=>{
+        const {count}=this.state
+        if(count===500){
+         return clearInterval(interval1)
+        }return this.setState(prev=>({count:prev.count+1}))
+    }
+
+    changeYears=()=>{
+        const {years}=this.state
+        if(years===10){
+         return clearInterval(interval2)
+        }return this.setState(prev=>({years:prev.years+1}))
+    }
+
+    changeExperience=()=>{
+        const {experience}=this.state
+        if(experience===5){
+         return clearInterval(interval3)
+        }return this.setState(prev=>({experience:prev.experience+1}))
+    }
+   
     render(){
+        const {count,years,experience}=this.state
         return(
     <div>
         <h1>Hello world</h1>
         <p>I had successfully published my code</p>
         <Image/>
         <Image2/>
+        <h1>{count} {years}  {experience}</h1>
     </div>
 )
         }
