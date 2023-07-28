201d106a2da5e036930fc84e438840fb0fe5d27d
nagasritha <eernagasritha@gmail.com>
2023-07-28 13:59:30 +0530

    replaced App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import logo from './logo.svg';
-import TrailComponentOne from './Components/TrailComponentOne'
-import './App.css';
+import TrailComponentOne from "./Components/TrailComponentOne";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TrailComponentOne/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=><TrailComponentOne/>
 
-export default App;
+export default App
