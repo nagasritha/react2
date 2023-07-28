264bb35444d304c2324c435397919c8df18bb2b7
nagasritha <eernagasritha@gmail.com>
2023-07-28 13:55:19 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import TrailComponentOne from './Components/TrailComponentOne'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <TrailComponentOne/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Components/TrailComponentOne/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const TrailComponentOne=()=>(
+    <div>
+        <h1>Hello world</h1>
+        <p>I had successfully published my code</p>
+    </div>
+)
+
+export default TrailComponentOne
